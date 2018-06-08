b43113097d7f2708ff622310d929420e6aa502bf
craigray <craig@craigrayner.com>
2018-06-08 12:42:19 +1000

    Doctrine Installation

---
+++ src/Listener/InstallListener.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 8/06/2018
+ * Time: 12:22
+ */
+namespace App\Listener;
+
+use App\Manager\InstallationManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class InstallListener implements EventSubscriberInterface
+{
+    /**
+     * getSubscribedEvents
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['installationCheck', 4],
+        ];
+    }
+
+    /**
+     * installationCheck
+     *
+     * @param GetResponseEvent $event
+     */
+    public function installationCheck(GetResponseEvent $event)
+    {
+        // Test for db installation.
+        $response = null;
+
+        // Are the database settings correct?
+        if (! $this->installationManager->testConnected())
+            $response = new RedirectResponse($this->router->generate('install_build'));
+
+        if (! is_null($response))
+            $event->setResponse($response);
+
+
+        return ;
+    }
+
+    /**
+     * @var InstallationManager
+     */
+    private $installationManager;
+
+    /**
+     * InstallListener constructor.
+     * @param InstallationManager $installationManager
+     */
+    public function __construct(InstallationManager $installationManager)
+    {
+        $this->installationManager = $installationManager;
+    }
+
+}
---
+++ src/Manager/InstallationManager.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 8/06/2018
+ * Time: 12:30
+ */
+namespace App\Manager;
+
+
+use Symfony\Component\Routing\RouterInterface;
+
+class InstallationManager
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * InstallationManager constructor.
+     * @param RouterInterface $router
+     */
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @return RouterInterface
+     */
+    public function getRouter(): RouterInterface
+    {
+        return $this->router;
+    }
+
+    /**
+     * Test Connected
+     *
+     * @param $params ['parameters']
+     *
+     * @return mixed
+     */
+    public function isConnected()
+    {
+        $this->connection = $this->getConnection(false);
+
+        $this->sql->error = 'No Error Detected.';
+        $this->sql->setConnected(true);
+
+        try {
+            $this->connection->connect();
+        } catch (ConnectionExceptio $e) {
+            $this->sql->error = $e->getMessage();
+            $this->sql->setConnected(false);
+            $this->exception = $e;
+        }
+
+        return $this->sql->isConnected();
+    }
+
+    /**
+     * @param bool $useDatabase
+     *
+     * @return \Doctrine\DBAL\Connection
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getConnection($useDatabase = true)
+    {
+        $config = new \Doctrine\DBAL\Configuration();
+
+        $connectionParams = [
+            'driver' => $this->sql->getDriver(),
+            'host' => $this->sql->getHost(),
+            'port' => $this->sql->getPort(),
+            'user' => $this->sql->getUser(),
+            'password' => $this->sql->getPass(),
+            'charset' => $this->sql->getCharset()
+        ];
+        if ($useDatabase)
+            $connectionParams['dbname'] = $this->sql->getName();
+
+        $this->connection = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+
+        return $this->connection;
+
+    }
+}
