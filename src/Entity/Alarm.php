2e9882253647ef24fb8985903f0a5caa1e31a588
craigray <craig@craigrayner.com>
2018-06-12 08:32:14 +1000

    Added Alarm Entity

---
+++ src/Entity/Alarm.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 11/06/2018
+ * Time: 16:39
+ */
+namespace App\Entity;
+
+class Alarm
+{
+    /**
+     * @var integer|null
+     */
+    private $id;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return Alarm
+     */
+    public function setId(?int $id): Alarm
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @var Person|null
+     */
+    private $person;
+
+    /**
+     * @return Person|null
+     */
+    public function getPerson(): ?Person
+    {
+        return $this->person;
+    }
+
+    /**
+     * @param Person|null $person
+     * @return Alarm
+     */
+    public function setPerson(?Person $person): Alarm
+    {
+        $this->person = $person;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     */
+    private $type;
+
+    /**
+     * @var array
+     */
+    public static $typeList = [
+        'general',
+        'lockdown',
+        'custom',
+        null,
+    ];
+
+    /**
+     * @return null|string
+     */
+    public function getType(): ?string
+    {
+
+        return $this->type = in_array($this->type, self::$typeList) ? $this->type : null ;
+    }
+
+    /**
+     * @param null|string $type
+     * @return Alarm
+     */
+    public function setType(?string $type): Alarm
+    {
+        $this->type = in_array($type, self::$typeList) ? $type : null ;;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     */
+    private $status;
+
+    /**
+     * @var array
+     */
+    public static $statusList = [
+        'current',
+        'past',
+    ];
+
+    /**
+     * @return null|string
+     */
+    public function getStatus(): ?string
+    {
+        return $this->status = in_array($this->status, self::$statusList) ? $this->status : 'past' ;
+    }
+
+    /**
+     * @param null|string $status
+     * @return Alarm
+     */
+    public function setStatus(?string $status): Alarm
+    {
+        $this->status = in_array($status, self::$statusList) ? $status : 'past' ;
+        return $this;
+    }
+
+    /**
+     * @var \DateTime|null
+     */
+    private $timestampStart;
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimestampStart(): ?\DateTime
+    {
+        return $this->timestampStart;
+    }
+
+    /**
+     * @param \DateTime|null $timestampStart
+     * @return Alarm
+     */
+    public function setTimestampStart(?\DateTime $timestampStart): Alarm
+    {
+        $this->timestampStart = $timestampStart;
+        return $this;
+    }
+
+    /**
+     * @var \DateTime|null
+     */
+    private $timestampEnd;
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimestampEnd(): ?\DateTime
+    {
+        return $this->timestampEnd;
+    }
+
+    /**
+     * @param \DateTime|null $timestampEnd
+     * @return Alarm
+     */
+    public function setTimestampEnd(?\DateTime $timestampEnd): Alarm
+    {
+        $this->timestampEnd = $timestampEnd;
+        return $this;
+    }
+}
