a90900902adb49ede585843d82a9e7b1d3ac866a
craigray <craig@craigrayner.com>
2018-06-11 21:52:50 +1000

    Added ActivitySlot

---
+++ src/Entity/ActivitySlot.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 11/06/2018
+ * Time: 16:39
+ */
+namespace App\Entity;
+
+class ActivitySlot
+{
+    /**
+     * @var integer|null
+     */
+    private $id;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return ActivitySlot
+     */
+    public function setId(?int $id): ActivitySlot
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     */
+    private $locationExternal;
+
+    /**
+     * @return null|string
+     */
+    public function getLocationExternal(): ?string
+    {
+        return $this->locationExternal;
+    }
+
+    /**
+     * @param null|string $locationExternal
+     * @return ActivitySlot
+     */
+    public function setLocationExternal(?string $locationExternal): ActivitySlot
+    {
+        $this->locationExternal = $locationExternal;
+        return $this;
+    }
+
+    /**
+     * @var \DateTime|null
+     */
+    private $timeStart;
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimeStart(): ?\DateTime
+    {
+        return $this->timeStart;
+    }
+
+    /**
+     * @param \DateTime|null $timeStart
+     * @return ActivitySlot
+     */
+    public function setTimeStart(?\DateTime $timeStart): ActivitySlot
+    {
+        $this->timeStart = $timeStart;
+        return $this;
+    }
+
+    /**
+     * @var \DateTime|null
+     */
+    private $timeEnd;
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getTimeEnd(): ?\DateTime
+    {
+        return $this->timeEnd;
+    }
+
+    /**
+     * @param \DateTime|null $timeEnd
+     * @return ActivitySlot
+     */
+    public function setTimeEnd(?\DateTime $timeEnd): ActivitySlot
+    {
+        $this->timeEnd = $timeEnd;
+        return $this;
+    }
+
+    /**
+     * @var Activity|null
+     */
+    private $activity;
+
+    /**
+     * @return Activity|null
+     */
+    public function getActivity(): ?Activity
+    {
+        return $this->activity;
+    }
+
+    /**
+     * @param Activity|null $activity
+     * @return ActivitySlot
+     */
+    public function setActivity(?Activity $activity): ActivitySlot
+    {
+        $this->activity = $activity;
+        return $this;
+    }
+
+    /**
+     * @var DayOfWeek|null
+     */
+    private $dayOfWeek;
+
+    /**
+     * @return DayOfWeek|null
+     */
+    public function getDayOfWeek(): ?DayOfWeek
+    {
+        return $this->dayOfWeek;
+    }
+
+    /**
+     * @param DayOfWeek|null $dayOfWeek
+     * @return ActivitySlot
+     */
+    public function setDayOfWeek(?DayOfWeek $dayOfWeek): ActivitySlot
+    {
+        $this->dayOfWeek = $dayOfWeek;
+        return $this;
+    }
+
+    /**
+     * @var Space|null
+     */
+    private $space;
+
+    /**
+     * @return Space|null
+     */
+    public function getSpace(): ?Space
+    {
+        return $this->space;
+    }
+
+    /**
+     * @param Space|null $space
+     * @return ActivitySlot
+     */
+    public function setSpace(?Space $space): ActivitySlot
+    {
+        $this->space = $space;
+        return $this;
+    }
+}
---
+++ src/Entity/DayOfWeek.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * This file is part of the Busybee Project.
+ *
+ * (c) Craig Rayner <craig@craigrayner.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * User: craig
+ * Date: 11/06/2018
+ * Time: 16:39
+ */
+namespace App\Entity;
+
+class DayOfWeek
+{
+    /**
+     * @var integer|null
+     */
+    private $id;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return DayOfWeek
+     */
+    public function setId(?int $id): DayOfWeek
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @var string|null
+     */
+    private $name;
+
+    /**
+     * @return null|string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param null|string $name
+     * @return DayOfWeek
+     */
+    public function setName(?string $name): DayOfWeek
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+}
