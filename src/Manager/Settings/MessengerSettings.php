765bc77b2258373730f2e6cea6b6b3e883d364f4
Craig Rayner <craig@craigrayner.com>
2018-10-04 14:24:00 +1000

    Messanger Settings Refactor

--- src/Manager/Settings/MessengerSettings.php
+++ src/Manager/Settings/MessengerSettings.php
@@ -22,8 +22,9 @@ use Hillrange\Form\Validator\Colour;
  * Class MessengerSettings
  * @package App\Manager\Settings
  */
-class MessengerSettings implements SettingCreationInterface
+class MessengerSettings extends SettingCreationManager
 {
+    use SMSSettings;
     /**
      * getName
      *
@@ -45,103 +46,57 @@ class MessengerSettings implements SettingCreationInterface
     public function getSettings(SettingManager $sm): SettingCreationInterface
     {
 
-        $sections = [];
-        $sections[] = self::getSMSSettings($sm);
-        $settings = [];
+        $this->setSettingManager($sm);
+        self::getSMSSettings($sm);
 
-        $setting = $sm->createOneByName('messenger.message_bubble_width_type');
 
-        $setting
-            ->__set('role', 'ROLE_PRINCIPAL')
-            ->setSettingType('choice')
-            ->__set('choice', ['regular', 'wide'])
-            ->setValidators(null)
-            ->__set('translateChoice', 'Setting')
+        $setting = $sm->createOneByName('messenger.message_bubble_width_type')
+            ->setSettingType('enum')
+            ->setChoices([
+                'messenger.message_bubble_width_type.regular' => 'regular',
+                'messenger.message_bubble_width_type.wide' => 'wide',
+            ])
             ->setDisplayName('Message Bubble Width Type')
-           ->setDescription('Should the message bubble be regular or wide?');
-        if (empty($setting->getValue())) {
-            $setting->setValue(null)
-            ;
-        }
-        $settings[] = $setting;
+            ->setDescription('Should the message bubble be regular or wide?');
+        if (empty($setting->getValue()))
+            $setting->setValue(null);
+        $this->addSetting($setting, []);
 
-        $setting = $sm->createOneByName('messenger.message_bubble_bgcolour');
-
-        $setting
-            ->__set('role', 'ROLE_PRINCIPAL')
+        $setting = $sm->createOneByName('messenger.message_bubble_bgcolour')
             ->setSettingType('string')
-
             ->setValidators(
                 [
                     new Colour(['enforceType' => 'rgba']),
                 ]
             )
-            ->__set('translateChoice', 'Setting')
             ->setDisplayName('Message Bubble Background Colour')
-           ->setDescription('Message bubble background colour in RGBA (e.g. 100,100,100,0.50). If blank, theme default will be used.');
-        if (empty($setting->getValue())) {
-            $setting->setValue(null)
-            ;
-        }
-        $settings[] = $setting;
+            ->setDescription('Message bubble background colour in RGBA (e.g. 100,100,100,0.50). If blank, theme default will be used.');
+        if (empty($setting->getValue()))
+            $setting->setValue(null);
+        $this->addSetting($setting, []);
 
-        $setting = $sm->createOneByName('messenger.message_bubble_auto_hide');
-
-        $setting->setName('messenger.message_bubble_auto_hide')
-            ->__set('role', 'ROLE_PRINCIPAL')
+        $setting = $sm->createOneByName('messenger.message_bubble_auto_hide')
             ->setSettingType('boolean')
             ->setDisplayName('Message Bubble Auto Hide')
-           ->setDescription('Should message bubble fade out automatically?');
-        if (empty($setting->getValue())) {
-            $setting->setValue(true)
+            ->setDescription('Should message bubble fade out automatically?');
+        if (empty($setting->getValue()))
+            $setting->setValue(true);
+        $this->addSetting($setting, []);
 
-                ->setValidators(null)
-                ->setDefaultValue(true)
-                ->__set('translateChoice', 'Setting')
-            ;
-        }
-        $settings[] = $setting;
-
-        $setting = $sm->createOneByName('messenger.enable_home_screen_widget');
-
-        $setting->setName('messenger.enable_home_screen_widget')
-            ->__set('role', 'ROLE_PRINCIPAL')
+        $setting = $sm->createOneByName('messenger.enable_home_screen_widget')
             ->setSettingType('boolean')
             ->setDisplayName('Enable Home Screen Widget')
-           ->setDescription('Adds a Message Wall widget to the home page, highlighting current messages.');
-        if (empty($setting->getValue())) {
-            $setting->setValue(false)
+            ->setDescription('Adds a Message Wall widget to the home page, highlighting current messages.');
+        if (empty($setting->getValue()))
+            $setting->setValue(false);
+        $this->addSetting($setting, []);
 
-                ->setValidators(null)
-                ->setDefaultValue(false)
-                ->__set('translateChoice', 'Setting')
-            ;
-        }
-        $settings[] = $setting;
+        $this->addSection('Message Wall Settings');
 
-        $section['name'] = 'Message Wall Settings';
-        $section['description'] = '';
-        $section['settings'] = $settings;
-        $section['colour'] = 'warning';
+        $this->setSectionsHeader('manage_messenger_settings');
 
-        $sections[] = $section;
+        $this->setSettingManager(null);
 
-        $sections['header'] = 'manage_messenger_settings';
-
-        $this->sections = $sections;
         return $this;
-    }
-
-    /**
-     * @var array
-     */
-    private $sections;
-
-    /**
-     * @return array
-     */
-    public function getSections(): array
-    {
-        return $this->sections;
     }
 }
